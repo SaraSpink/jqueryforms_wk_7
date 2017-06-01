303018bde9af06715805be4c5424c0c801474232
Sara Spink and Tory Bryant <sara@>
2017-05-31 19:06:39 -0700

    add a shipping information form

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var person1Input= $("input#person1").val();
+    var address1Input= $("input#address1").val();
+    var address2Input= $("input#address2").val();
+    var cityInput= $("input#city").val();
+    var zipInput= $("input#zip").val();
 
-    $(".person1").text(person1Input);
+    $(".person1").text(person1Input.toUpperCase());
+    $(".address1").text(address1Input);
+    $(".address2").text(address2Input);
+    $(".city").text(cityInput);
+    $(".zip").text(zipInput);
 
     $(".jumbotron").show();
     $("#letter").show();
